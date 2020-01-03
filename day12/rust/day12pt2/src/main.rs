479de71142a7fc841dee9e0dc3f36a0bd28f4338
HeadBeeGuy <jake.alt.dev@gmail.com>
2020-01-03 23:16:47 +0900

    Add solution for Day 12 part 2

--- day12/rust/day12pt2/src/main.rs
+++ day12/rust/day12pt2/src/main.rs
@@ -1,16 +1,21 @@
 // Advent of Code Day 12 part 2
 
-// This is my initial work on Day 12 part 2. It should work in theory, but 
-// running in on the second test case shows that it's deliberately designed to
-// make this computation take forever.
-
-// I got the idea that the simulation will return to its initial state in a cycle
-// from this blog post. I'll have to look at simulating one dimension at a time
-// at some point soon.
+// I did something I did a few times last year, and that's look at an existing
+// solution on a blog post before writing my own. Here's what I based this on:
 // https://dhconnelly.com/advent-of-code-2019-commentary.html#day-12
 
-// Right now I think I'll work on something else!
+// It's always kind of a bummer to base my solution on someone else's, even if
+// they're a pro and making their solution freely available. I wish I were clever
+// enough to come up with this on my own!
 
+// I originally used the naive original implementation from part 1, but in true
+// Advent of Code fashion, it would have taken an absurd amount of time. My final
+// answer was something like 500 trillion and even this far more efficient method
+// takes about a second and a half on my laptop. I haven't learned about threads
+// or async in Rust yet, so maybe I could speed this up by running the three
+// calculations in parallel.
+
+use num::Integer;
 use regex::Regex;
 use std::{env, fs, process};
 
@@ -24,40 +29,7 @@ struct Moon {
     v_z: i32,
 }
 
-impl Moon {
-    fn apply_pull_from_other_moon(
-        &mut self,
-        other_moon_x: i32,
-        other_moon_y: i32,
-        other_moon_z: i32,
-    ) {
-        if self.pos_x < other_moon_x {
-            self.v_x += 1;
-        } else if self.pos_x > other_moon_x {
-            self.v_x -= 1;
-        }
-
-        if self.pos_y < other_moon_y {
-            self.v_y += 1;
-        } else if self.pos_y > other_moon_y {
-            self.v_y -= 1;
-        }
-
-        if self.pos_z < other_moon_z {
-            self.v_z += 1;
-        } else if self.pos_z > other_moon_z {
-            self.v_z -= 1;
-        }
-    }
-
-    fn update_velocity(&mut self) {
-        self.pos_x += self.v_x;
-        self.pos_y += self.v_y;
-        self.pos_z += self.v_z;
-    }
-}
-
-fn process_arguments(args: &[String]) -> Result<Vec<Moon>, &str> {
+fn process_arguments(args: &[String]) -> Result<(Vec<i32>, Vec<i32>, Vec<i32>), &str> {
     if args.len() != 2 {
         return Err("Please specify a text file for the moon data.");
     }
@@ -65,7 +37,9 @@ fn process_arguments(args: &[String]) -> Result<Vec<Moon>, &str> {
         Regex::new(r"<[xyz]=(-?[0-9]+),\s[xyz]=(-?[0-9]+),\s[xyz]=(-?[0-9]+)>").unwrap();
     let seed_file = fs::read_to_string(&args[1]).expect("The file path was not valid.");
 
-    let mut moons: Vec<Moon> = Vec::new();
+    let mut x_coordinates: Vec<i32> = Vec::new();
+    let mut y_coordinates: Vec<i32> = Vec::new();
+    let mut z_coordinates: Vec<i32> = Vec::new();
 
     for cap in moon_format.captures_iter(&seed_file) {
         let new_x = &cap[1]
@@ -78,55 +52,64 @@ fn process_arguments(args: &[String]) -> Result<Vec<Moon>, &str> {
             .parse::<i32>()
             .expect("A moon had an invalid starting value in its position.");
 
-        moons.push(Moon {
-            pos_x: *new_x,
-            pos_y: *new_y,
-            pos_z: *new_z,
-            v_x: 0,
-            v_y: 0,
-            v_z: 0,
-        });
+        x_coordinates.push(*new_x);
+        y_coordinates.push(*new_y);
+        z_coordinates.push(*new_z);
     }
-    Ok(moons)
+    Ok((x_coordinates, y_coordinates, z_coordinates))
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut moons: Vec<Moon> = process_arguments(&args).unwrap_or_else(|err| {
-        println!("Argument error: {}", err);
-        process::exit(1);
-    });
+fn steps_until_axis_repeats(moon_positions: &Vec<i32>) -> i64 {
+    // Assumptions:
+    // Every moon starts with zero velocity.
+    // There are four moons.
 
-    let initial_moon_state = moons.clone();
-
-    let mut steps: i128 = 0;
+    let mut steps: i64 = 0;
+    let initial_positions = moon_positions.clone();
+    let mut current_positions = moon_positions.clone();
+    let mut moon_velocities: Vec<i32> = vec![0, 0, 0, 0];
 
     loop {
         steps += 1;
 
-        for current_moon in 0..=3 {
-            for other_moon in 0..=3 {
-                let other_moon_x = moons[other_moon].pos_x;
-                let other_moon_y = moons[other_moon].pos_y;
-                let other_moon_z = moons[other_moon].pos_z;
-
+        // apply gravitational pull from other moons
+        for current_moon in 0..4 {
+            for other_moon in 0..4 {
                 if !(current_moon == other_moon) {
-                    moons[current_moon].apply_pull_from_other_moon(
-                        other_moon_x,
-                        other_moon_y,
-                        other_moon_z,
-                    );
+                    if current_positions[current_moon] < current_positions[other_moon] {
+                        moon_velocities[current_moon] += 1;
+                    } else if current_positions[current_moon] > current_positions[other_moon] {
+                        moon_velocities[current_moon] -= 1;
+                    }
                 }
             }
         }
 
-        for moon in &mut moons {
-            moon.update_velocity();
+        for current_moon in 0..4 {
+            current_positions[current_moon] += moon_velocities[current_moon];
         }
 
-        if moons == initial_moon_state {
-            println!("The moons have returned to their initial state at step {}.", steps);
+        if (current_positions == initial_positions) && (moon_velocities == vec![0, 0, 0, 0]) {
             break;
         }
     }
+    steps
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let (x_coordinates, y_coordinates, z_coordinates) =
+        process_arguments(&args).unwrap_or_else(|err| {
+            println!("Argument error: {}", err);
+            process::exit(1);
+        });
+
+    let x = steps_until_axis_repeats(&x_coordinates);
+    let y = steps_until_axis_repeats(&y_coordinates);
+    let z = steps_until_axis_repeats(&z_coordinates);
+
+    println!(
+        "History repeats itself. In this case, it will take {} steps.",
+        x.lcm(&(y.lcm(&z)))
+    );
 }
